85b2e25afed138eac3f6cb820cfa43fb8a95dbf3
manbodhvikram <manbodhvikram@gmail.com>
2023-06-21 14:36:25 +0530

    encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
-class Duck{
+ class Person {
     private String name;
+    private int age;
 
-    public Duck(String name) {
-        this.name = name;
-    }
-
+    // Getter method for name
     public String getName() {
         return name;
     }
 
+    // Setter method for name
     public void setName(String name) {
         this.name = name;
     }
+
+    // Getter method for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter method for age
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Duck d=new Duck("babo");
-        //d.setName();
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
 
-
-        System.out.println(d.getName());
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
     }
 }
